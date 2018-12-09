2c441a5e5cdf461adcfa52dbbff360ac09aacabe
PatrickHickey <codewithpat@gmail.com>
2018-12-08 22:56:34 -0800

    Added object types

--- schema.js
+++ schema.js
@@ -19,3 +19,11 @@ const LaunchType = new GraphQLObjectType({
 });
 
 // Rocket Type
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
+});
